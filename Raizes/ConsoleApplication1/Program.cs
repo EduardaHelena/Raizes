fb199625247c0129b2d9cea9fc4d54f7899b5c5c
Eduarda Helena <dudinhahelenaa@gmail.com>
2016-07-15 09:12:40 -0300

    Raizes

---
+++ Raizes/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raiz p = new Raiz();
+
+            Console.WriteLine("Até que numero você deseja?");
+            try
+            {
+                p.limit = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERR0!!!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            
+
+            int sqrt = p.getSQRT(p.limit);
+            Console.WriteLine(sqrt);
+
+            //float sqrt2 = p.getSQRT2(p.limit);
+            //Console.WriteLine(sqrt2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Raizes/ConsoleApplication1/Raiz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raizes
+{
+    class Raiz
+    {
+        public int limit = 100;
+        
+        public int getSQRT(int limit)
+        {
+            int count = 0;
+            int currentImpar = 1;
+            while (limit - currentImpar >= 0)
+            {
+                count++;
+                limit -= currentImpar;
+                currentImpar += 2;
+            }
+            return count;
+        }
+
+        //public float getSQRT2(int limit)
+        //{
+        //    int soma = 0;
+        //    int currentImpar = 1;
+        //    int currentNumber = 0;
+        //    while (soma + currentImpar <= limit)
+        //    {
+        //        soma += currentImpar;
+        //        currentImpar += 2;
+        //        currentNumber++;
+
+        //        Console.WriteLine("Soma: " + soma);
+        //    }
+
+            
+        //    return currentNumber;
+        //}
+    }
+}
+
+// 1, 3, 5, 7, 9
